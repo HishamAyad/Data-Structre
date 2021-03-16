b8b53adca3d1244a6da9b1d2a2fa2315776a373a
HishamAyad <66042145+HishamAyad@users.noreply.github.com>
2021-03-16 17:48:26 +0200

    imp.

---
+++ a/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double x,y,sum;
+    cin>>x>>y;
+    sum=x+y;
+    cout<<fixed<<setprecision(5)<<sum<<endl;
+    return 0;
+}
